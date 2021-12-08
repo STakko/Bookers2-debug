16763c1e40e84baa72d9f4879154e2f48ddb5787
EC2 Default User <ec2-user@ip-172-31-86-126.ec2.internal>
2021-12-08 09:03:00 +0000

    [Fix]oyo9d

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
+  
+
   resources :users, only: [:show,:index,:edit,:update] do
     get 'day_search', to: 'users#day_search'
     member do #meberを使うことで別のルーティングも追加できる
